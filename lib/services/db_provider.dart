ce94d73a6b9f849510923c621ea4ee1456db6c67
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-20 09:46:38 +0700

    update db provider

--- lib/services/db_provider.dart
+++ lib/services/db_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter_fam_expenseslog/model/expense_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
@@ -21,21 +22,37 @@ class DBProvider {
 
   initDB() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
-    List<String> listPath = [documentDirectory.path, "TestDB.db"];
-    String path = listPath.join();
+
+    // List<String> listPath = [documentDirectory.path, "TestDB.db"];
+    // String path = listPath.join();
+
+    String path = documentDirectory.path + "TestDB.db";
 
     return await openDatabase(
       path,
       version: 1,
       onOpen: (db) {},
       onCreate: (db, version) async {
-        await db.execute("CREATE TABLE Client ("
+        await db.execute("CREATE TABLE expense2 ("
             "id INTEGER PRIMARY KEY,"
-            "first_name TEXT,"
-            "last_name TEXT,"
-            "blocked BIT,"
+            "title TEXT,"
+            "desc TEXT,"
+            "date DATETIME,"
             ")");
       },
     );
+  }
+
+  createDB2(ExpenseModel expenseModel) async {
+    final db = await database;
+    var res = await db.rawInsert("INSERT into expense2 (title, desc, date)"
+        "VALUES (${expenseModel.title}, ${expenseModel.desc}, ${expenseModel.date})");
+    return res;
+  }
+
+  createDB(ExpenseModel expenseModel) async {
+    final db = await database;
+    var res = await db.insert("expense2", expenseModel.toJson());
+    return res;
   }
 }
