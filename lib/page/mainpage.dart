5c9b111bc1ff05414419f78b3b8db6034e8a9e94
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-04 07:40:17 +0700

    update main page

--- lib/page/mainpage.dart
+++ lib/page/mainpage.dart
@@ -50,10 +50,10 @@ class _MainPageState extends State<MainPage> {
             vertical: screenUtil.setHeight(5),
             horizontal: screenUtil.setWidth(20),
           ),
-          // child: StreamBuilder<List>(
-          //     stream: DBProvider.dbProvider.streamData(),
-          child: FutureBuilder(
-              future: DBProvider.dbProvider.selectAllData(),
+          child: StreamBuilder<List<ExpenseModel>>(
+              stream: DBProvider.dbProvider.streamData(),
+          // child: FutureBuilder(
+          //     future: DBProvider.dbProvider.selectAllData(),
               builder: (BuildContext context,
                   AsyncSnapshot<List<ExpenseModel>> snapshot) {
                 // print(snapshot.data);
